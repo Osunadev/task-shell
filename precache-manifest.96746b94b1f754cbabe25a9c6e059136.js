f2547639e7e14d0a9c86b707e3a2fa4a5a04b84a
Omar Osuna <omar.alonso.osuna.angulo@uabc.edu.mx>
2020-07-07 14:51:42 -0700

    Updates

---
+++ precache-manifest.96746b94b1f754cbabe25a9c6e059136.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869e41b5ab6c217d809d2b6de66a3502",
+    "url": "/task-shell/index.html"
+  },
+  {
+    "revision": "57fb45c1c42cd622462c",
+    "url": "/task-shell/static/css/main.08a01c0b.chunk.css"
+  },
+  {
+    "revision": "b32a1aac60a66000c358",
+    "url": "/task-shell/static/js/2.6c0811e3.chunk.js"
+  },
+  {
+    "revision": "462f991c1d2ef16102b314f225132ac4",
+    "url": "/task-shell/static/js/2.6c0811e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57fb45c1c42cd622462c",
+    "url": "/task-shell/static/js/main.340817c1.chunk.js"
+  },
+  {
+    "revision": "35f5666ff885ae627e43",
+    "url": "/task-shell/static/js/runtime-main.733c9532.js"
+  },
+  {
+    "revision": "22c595c04db22bb0363d711f5c1cd486",
+    "url": "/task-shell/static/media/Alatsi-Regular.22c595c0.ttf"
+  },
+  {
+    "revision": "851cb39977f22effd39c5cffd6a04c9b",
+    "url": "/task-shell/static/media/FiraCode-Regular.851cb399.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/task-shell/static/media/Lato-Regular.2d36b1a9.ttf"
+  }
+]);
